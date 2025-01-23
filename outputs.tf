19559b567ac7d7e299c6153f7ab54ee559ce8d85
RahulYadav22775 <gopichand556677@gmail.com>
2025-01-23 16:30:33 +0530

    upgrade

--- outputs.tf
+++ outputs.tf
@@ -8,13 +8,6 @@ output "vpc_id" {
   value = aws_vpc.main.id   
 }
 
-output "az_info" {
-
-    value = data.aws_availability_zones.az_info
-}
-
-
-
 output "public_subnet_ids"{
   value = aws_subnet.public_subnets[*].id
 }
